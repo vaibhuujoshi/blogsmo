b6801157995db095e7e553154dd0a6519ed55d1a
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-06-05 19:27:00 +0530

    Add: route user auth endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from 'hono'
+import userRouter from './routes/user';
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.route('/api/v1', userRouter);
 
 export default app
