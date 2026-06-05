d1f32ab0dc3ae093e0fe95094e074216698ffd09
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-06-05 19:25:17 +0530

    Fix: installed @prisma/client

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -2,6 +2,6 @@ import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "./generated/prisma/client";
 
 export const getPrismaClient = (databaseUrl: string) => {
-    const adapter = new PrismaNeon({connectionString: databaseUrl});
-    return new PrismaClient({adapter});
+    const adapter = new PrismaNeon({ connectionString: databaseUrl });
+    return new PrismaClient({ adapter });
 }
