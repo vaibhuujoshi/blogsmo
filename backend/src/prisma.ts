5e310e5f857b740330967663e4c587704b1e92f0
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-06-07 17:06:53 +0530

    Remove filename.txt

--- backend/src/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "./generated/prisma/client";
-
-export const getPrismaClient = (databaseUrl: string) => {
-    const adapter = new PrismaNeon({ connectionString: databaseUrl });
-    return new PrismaClient({ adapter });
-}
