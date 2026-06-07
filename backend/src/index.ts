33cb1708c4bbbcf55a24b29444d2f06dbac9d4ee
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-06-07 16:51:47 +0530

    Add: prismaClient middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,28 @@
 import { Hono } from 'hono'
 import userRouter from './routes/user';
 import blogRouter from './routes/blog';
+import { getPrismaClient } from './prisma';
 
-const app = new Hono<{
+type Env = {
   Bindings: {
     DATABASE_URL: string,
     JWT_SECRET: string,
+  },
+  Variables: {
+    prisma: prismaType
   }
-}>();
+}
+
+export type prismaType = ReturnType<typeof getPrismaClient>;
+
+const app = new Hono<Env>();
+
+app.use('*', async (c, next) => {
+  const prismaInstance = getPrismaClient(c.env.DATABASE_URL)
+
+  c.set('prisma', prismaInstance);
+  await next()
+})
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
