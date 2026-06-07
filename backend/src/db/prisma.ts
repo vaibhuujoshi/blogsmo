a869900fcf5e52c107fc87fae0ad149c132e21e4
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-06-07 17:42:32 +0530

    Fix: move prisma file to db

---
+++ backend/src/db/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "../generated/prisma/client";
+
+export const getPrismaClient = (databaseUrl: string) => {
+    const adapter = new PrismaNeon({ connectionString: databaseUrl });
+    return new PrismaClient({ adapter });
+}
